4cf0b05a97f67536fef1f2b3f0335a2ea45ca86a
Ryu <ryujt658@hanmail.net>
2020-02-08 18:41:13 +0900

    애니메이션 효과 제거, 스레드 우선 순위 높임

--- src/server/remo_server.dpr
+++ src/server/remo_server.dpr
@@ -1,6 +1,10 @@
 program remo_server;
 
 uses
+  Windows,
+  ThreadUtils,
+  AnimationControl,
+  DrawFullWindow,
   Vcl.Forms,
   _fmMain in '_fmMain.pas' {fmMain},
   Config in '..\..\lib\Config.pas',
@@ -17,8 +21,17 @@ uses
 {$R *.res}
 
 begin
+  SetPriorityClass( GetCurrentProcess, REALTIME_PRIORITY_CLASS );
+  SetThreadPriorityFast;
+
+  DisableAnimation;
+  DisableDrawFullWindow;
+
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TfmMain, fmMain);
   Application.Run;
+
+  RestoreAnimation;
+  RestoreDrawFullWindow;
 end.
